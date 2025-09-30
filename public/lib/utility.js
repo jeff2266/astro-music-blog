0833275e88d556b46d4995cd7a475c7a6bd285fc
Jeff <jeff2266@gmail.com>
2025-09-30 14:32:38 -0700

    .

--- public/lib/utility.js
+++ public/lib/utility.js
@@ -1,28 +1,9 @@
 function SoundCloudManager() {
-    // null if widget is not loaded
-    let state = null;
-    let widget = null;
+    let state = "error";
+    let widget = SC.Widget("sc-player");
 
-    async function getWidget(url) {
-        if (!isValidMixUrl(url)) {
-            throw new Error('Invalid SoundCloud URL');
-        }
-        const urlObject = new URL('https://soundcloud.com/oembed');
-        urlObject.searchParams.set("url", sanitizeUrl(url));
-        urlObject.searchParams.set("auto_play", "true");
-        urlObject.searchParams.set("maxheight", "166");
-
-        const result = await fetch(urlObject.toString())
-        const resp = await result.json()
-        return resp.html
-    }
-
-    function bindEvents() {
-        widget.bind(SC.Widget.Events.READY, function () {
-            console.log("SoundCloud Widget is ready");
-            state = "ready";
-        });
-
+    widget.bind(SC.Widget.Events.READY, function () {
+        state = "ready";
         widget.bind(SC.Widget.Events.ERROR, function () {
             console.log("SoundCloud Widget is error");
             state = "error";
@@ -32,7 +13,7 @@ function SoundCloudManager() {
             console.log("SoundCloud Widget is finished");
             state = "finished";
         });
-    }
+    });
 
     function isValidMixUrl(url) {
         const urlObject = url ? new URL(url) : null;
@@ -54,18 +35,13 @@ function SoundCloudManager() {
     }
 
     this.setCurrentMix = async function (url) {
-        // Check if widget exists
-        if (state === null) {
-            const c = document.getElementById("sc-player-container");
-            if (c) {
-                c.innerHTML = await getWidget(url);
-                c.firstElementChild.id = "sc-player";
-                widget = SC.Widget("sc-player");
-                bindEvents();
-            }
-        } else {
-            widget.load(sanitizeUrl(url), { auto_play: true });
+        const sanitizedUrl = sanitizeUrl(url);
+        if (!isValidMixUrl(sanitizedUrl)) {
+            console.error('Invalid SoundCloud URL:', url);
+            return;
         }
+        document.getElementById('sc-player-container').classList.remove('hidden');
+        widget.load(sanitizedUrl, { auto_play: true, show_comments: false });
     }
 
     this.getState = function () { return state; }
